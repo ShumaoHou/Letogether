16cf5a6d60ab748ffd72ec367781bc64cb777641
HoHo <houshumao@qq.com>
2019-01-21 20:52:39 +0800

    '我创建的协游'中可以删除项目

--- miniprogram/pages/more_list/more_list.js
+++ miniprogram/pages/more_list/more_list.js
@@ -7,26 +7,19 @@ Page({
   data: {
     feed: [],
     feed_length: 0,
+    deleteModalHidden: true,// 对话框隐藏
     list_type: 0, // 项目列表类型。0：我创建的协游；1：我参加的协游；2：协游加入申请。
+    _id: "",// 点击的event _id
   },
   /**
-   * 点击函数--项目列表
-   */
-  bindEventTap: function(e) {
-    var id = e.currentTarget.dataset.id // 传递数据库_id
-    console.log("_id:", id)
-    wx.navigateTo({
-      url: '../event/event?id=' + id
-    })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(e) {
+ * 生命周期函数--监听页面加载
+ */
+  onLoad: function (e) {
     var type = e.type
     if (type == "created") { // 我创建的协游
       this.setData({
         list_type: 0,
+        deleteHidden: false,
       })
       wx.setNavigationBarTitle({
         title: '我创建的协游',
@@ -50,34 +43,39 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
-    wx.showNavigationBarLoading() // 显示导航栏加载
+  onShow: function () {
     console.log('onLoad')
-    var that = this
+    this.queryAllEvent()
+  },
+  /**
+   * 查询数据库并更新event列表
+   */
+  queryAllEvent: function(){
+    wx.showNavigationBarLoading() // 显示导航栏加载
     wx.cloud.callFunction({
       name: 'queryAllEvent',
       data: {
-        type: that.data.list_type,
+        type: this.data.list_type,
       },
       complete: res => {
         if (res.result.query) { // 如果存在数据
-          var type = that.data.list_type
+          var type = this.data.list_type
           var feedData = []
           var resData = res.result.queryRes.data
           for (var i = 0; i < resData.length; i++) {
             if (type == 0) {// 我创建的协游
-              if (resData[i].event.actors[0] == app.globalData.userInfo.openid){
+              if (resData[i].event.actors[0] == app.globalData.userInfo.openid) {
                 feedData.push(resData[i])
               }
             } else if (type == 1) {// 我参加的协游
-              if (resData[i].event.actors.indexOf(app.globalData.userInfo.openid)>0) {
+              if (resData[i].event.actors.indexOf(app.globalData.userInfo.openid) > 0) {
                 feedData.push(resData[i])
               }
             } else if (type == 2) {// 协游加入申请
 
             }
           }
-          that.setData({
+          this.setData({
             feed: feedData,
           });
           console.log("querAllEvent:", res.result.queryRes.data)
@@ -86,5 +84,52 @@ Page({
       }
     })
   },
-
+  /**
+   * 点击函数--项目列表
+   */
+  bindEventTap: function(e) {
+    var id = e.currentTarget.dataset.id // 传递数据库_id
+    console.log("_id:", id)
+    wx.navigateTo({
+      url: '../event/event?id=' + id
+    })
+  },
+  /**
+   * 点击函数--项目删除
+   */
+  bindDeleteTap: function(e) {
+    var id = e.currentTarget.dataset.id // 传递数据库_id
+    this.data._id = id
+    console.log("Delete _id:", id)
+    if (this.data.list_type == 0){
+      this.setData({
+        deleteModalHidden: false,
+      })
+    }
+  },
+  /**
+   * 对话框--删除确认
+   */
+  bindDeleteConfirm: function (e) {
+    wx.cloud.callFunction({
+      name: 'deleteEvent',
+      data: {
+        _id: this.data._id,
+      },
+      complete: res => {
+        this.queryAllEvent()
+      }
+    })
+    this.setData({
+      deleteModalHidden: true,
+    })
+  },
+  /**
+   * 对话框--删除取消
+   */
+  bindDeleteCancel: function(e) {
+    this.setData({
+      deleteModalHidden: true,
+    })
+  },
 })
