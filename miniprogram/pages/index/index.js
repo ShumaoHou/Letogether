f7e090a6a1c0462fabe677d580ef95bb378e7ec4
HoHo <houshumao@qq.com>
2019-03-09 23:35:07 +0800

    添加手动播放背景音乐的功能

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,9 +2,15 @@
 var desData = require('../../data/data_des.js'); // 目的地信息json数据
 var app = getApp()
 
+const innerAudioContext = wx.createInnerAudioContext()
+
 Page({
   data: {
     feed: [],
+    // 背景音乐
+    mp3: "http://www.ytmp3.cn/down/58810.mp3",
+    bgm: false,
+    animationData: {},  // 播放按钮转动动画
   },
   /**
    * 项目点击函数
@@ -51,5 +57,52 @@ Page({
         })
       }
     })
+    // 播放按钮转动动画
+    var animation = wx.createAnimation({
+      duration: 1000,
+      timingFunction: 'ease',
+    })
+    this.animation = animation
+    this.setData({
+      animationData: animation.export()
+    })
+    var n = 0;
+    var m = true;
+    //连续动画需要添加定时器,所传参数每次+1就行
+    setInterval(function () {
+      n = n + 1;
+      if (m) {
+        this.animation.rotate(360 * (n)).scale(1.2, 1.2).step()
+        m = !m;
+      } else {
+        this.animation.rotate(360 * (n)).scale(1, 1).step()
+        m = !m;
+      }
+      this.setData({
+        animationData: this.animation.export()
+      })
+    }.bind(this), 1000)
+
+  },
+  /**
+   * 背景音乐播放按钮
+   */
+  bindPlayInnerAudio: function() {
+    let that = this;
+    if (that.data.bgm) {
+      that.setData({
+        bgm: false,
+      })
+      innerAudioContext.pause(); /**  暂停音乐 */
+    } else {
+      that.setData({
+        bgm: true,
+      })
+      // 播放音乐
+      innerAudioContext.autoplay = true
+      innerAudioContext.loop = true
+      innerAudioContext.src = that.data.mp3;
+      innerAudioContext.play()
+    }
   },
 })
